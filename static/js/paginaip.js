6ae818c8b563971446c9feb410eb74b61731d9fe
dalailama <raulbarbosa781@gmail.com>
2025-06-03 13:46:00 -0300

    .

--- static/js/paginaip.js
+++ static/js/paginaip.js
@@ -1,6 +1,6 @@
 
-const ipPython = "http://10.92.3.149:5000"
-const ipPython2 = "http://10.92.3.149:5000"
+const ipPython = "http://192.168.1.121:5000"
+const ipPython2 = "http://192.168.1.121:5000"
 // ESSA PÁGINA É ONDE FICA SALVA A VARIÁVEL RESPONSÁVEL POR
 // ARMAZENAR O URL DO IP DO SERVIDOR
 
